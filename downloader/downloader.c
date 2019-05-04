b3a8d2a4e210fce5240e646ac080797b7b156384
David <davidgracia99@gmail.com>
2019-05-04 21:31:04 +0200

    añade descarga

--- downloader/downloader.c
+++ downloader/downloader.c
@@ -4,7 +4,7 @@
 #include <sys/wait.h>
 #include <sys/types.h>//biblioteca para el wait
 //#define URL_OBJETIVO "http://212.128.69.216/lolo" //SUSTITUIR POR ARCHIVO LOCAL PEQUEÑO
-#define URL_OBJETIVO archivo
+#define URL_OBJETIVO "archivo"
 //#define TAMANNO_OBJETIVO_REMOTO_BYTES 1047491658L 
 #define TAMANNO_OBJETIVO_REMOTO_BYTES 5868L //TAMAÑO ARCHIVO LOCAL d3fa600db6d81dcd94b531315c60a433
 #define PREFIJO_NOMBREARCHIVO_FRAGMENTO "descarga"
@@ -29,10 +29,12 @@ int valida_argumentos(int numero_argumento, char* argumentos[]) {
     return(error);
 }
 void descarga_fragmento(char* url, long desde, long hasta, char* archivo_salida) {
+    /**Ejemplo llamada curl :
+    * curl -s -H "Range: bytes=2-3" https://localhost/testfile.txt -o nombrearchivo */ //no vale rango
     char rango[200];
-    sprintf(rango, "Range: bytes=%ld-%ld", desde, hasta);//no vale rango
+    sprintf(rango, "Range: bytes=%ld-%ld", desde, hasta);//no vale rango!
     printf("Probando %s\n",rango);
-    execl("curl", "curl", "-s", "-H", rango, url, "-o", archivo_salida, NULL);
+    execlp("curl", "curl", "-s", "-H", rango, url, "-o", archivo_salida, NULL);
     perror("Error"); //no deberia llegar hasta aqui ya que execlp reemplaza el codigo del proceso
 }
 int main(int argumento_numero, char* argumentos[]) {
@@ -45,7 +47,7 @@ int main(int argumento_numero, char* argumentos[]) {
     if (TAMANNO_OBJETIVO_REMOTO_BYTES % numero_procesos == 0) {printf ("%d fragmentos de %d bytes\n",numero_procesos,tamanno_fragmento);}
     else {
         printf ("%d fragmentos de %d bytes\n", numero_procesos-1, tamanno_fragmento);
-        printf ("%d fragmentos de %ld bytes\n", 1, TAMANNO_OBJETIVO_REMOTO_BYTES-(tamanno_fragmento*numero_procesos)+tamanno_fragmento);
+        printf ("%d fragmento de %ld bytes\n", 1, TAMANNO_OBJETIVO_REMOTO_BYTES-(tamanno_fragmento*numero_procesos)+tamanno_fragmento);
     }
     printf ("Total %ld bytes a descargar \n", TAMANNO_OBJETIVO_REMOTO_BYTES);
     
@@ -60,9 +62,6 @@ int main(int argumento_numero, char* argumentos[]) {
             desde = hasta + 1;
             hasta = TAMANNO_OBJETIVO_REMOTO_BYTES;
         }
-        
-        printf("\t chunk #%d: Range %d-%d \n", proceso_numero, desde, hasta);
-        
         /** TODO: Crea el proceso hijo que hara:
          *   - Escribe un mensaje mostrando el fragmento que descargara (sobre todo para propositos de depuracion
          *  (p.ej. printf("\t fragmento #%d: Rango %d-%d \n", proceso_numero, desde, hasta))
@@ -71,10 +70,24 @@ int main(int argumento_numero, char* argumentos[]) {
          *       sprintf(archivo_salida, "%s-%d", PREFIJO_NOMBREARCHIVO_FRAGMENTO, proceso_numero);
          *   - Llama a descarga_fragmento(URL_OBJETIVO, desde, hasta, archivo_); 
          *   - exit(0);*/
+        
+        pid = fork();
+        if (pid == 0) { //Este codigo sera ejecutado por el proceso hijo
+            printf("hijo #%d: Hola!\n",proceso_numero);
+            printf("hijo #%d: Hecho! Hasta la vista\n",proceso_numero);
+            descarga_fragmento(URL_OBJETIVO, desde, hasta, archivo_salida); 
+            return(0); //El return aqui terminara la ejecucion del hijo
+        }
+        sprintf(archivo_salida, "%s-%d", PREFIJO_NOMBREARCHIVO_FRAGMENTO, proceso_numero);
+        printf("%s-%d", PREFIJO_NOMBREARCHIVO_FRAGMENTO, proceso_numero);
+        printf("\t fragmento #%d: Rango %d-%d \n", proceso_numero, desde, hasta);
+        
         if (modo_descarga == 'S') {
             /**TODO: el padre debe esperar hasta que el hijo haya terminado de
              * descargar el fragmento actual si el modo de descarga es S
              * (sequencial)*/
+            printf("padre: esperando a que el hijo #%d termine\n",proceso_numero);
+            wait(&estado);
         }
         if (modo_descarga == 'P') {
             /*for (proceso_numero=1; proceso_numero <= numero_procesos; proceso_numero++) { //codigo del hijo
@@ -89,10 +102,10 @@ int main(int argumento_numero, char* argumentos[]) {
         printf("%s-%d", PREFIJO_NOMBREARCHIVO_FRAGMENTO, proceso_numero);
         printf("\t fragmento #%d: Rango %d-%d \n", proceso_numero, desde, hasta);*/
     }
-    printf ("-- Finaliza padre y downloader --\n");
+    printf ("-- Termina padre --\n");
+    printf ("-- Termina downloader --\n");
     //exit(0);
 }
-/**Ejemplo llamada curl :
- * curl -s -H "Range: bytes=2-3" https://localhost/testfile.txt -o nombrearchivo */ //no vale rango
+
 
 
